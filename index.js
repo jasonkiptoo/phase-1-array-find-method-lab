cc746c1e09a8cc4843c9ae94b91c4635254ddbd0
jasonkiptoo <kiptoojason@gmail.com>
2022-08-22 08:28:18 +0300

    Completed assignmnent

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record){
+
+
+    const winningYear= record.find(records=>records.result==="W");
+   
+   
+  if( winningYear){
+    return winningYear.year
+  }
+
+}
