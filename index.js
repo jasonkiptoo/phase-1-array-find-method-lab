1a27d943731ed5f3427b05323186d51cb8e98faf
jasonkiptoo <kiptoojason@gmail.com>
2022-08-22 13:55:51 +0300

    Completed assignmnent

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// code your solution here
 
 function superbowlWin(record){
 
-
-    const winningYear= record.find(records=>records.result==="W");
-   
-   
-  if( winningYear){
-    return winningYear.year
-  }
-
+  let yearWin=record.find(record=>record.result==="W")
+if(yearWin){
+  return yearWin.year
+}
+ 
 }
